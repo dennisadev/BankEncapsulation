7cc609c6310575932cbb7f6832f90249ffb8fde5
Dennis <dennisadev@gmail.com>
2024-12-10 16:05:06 -0500

    inital commit

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        //Feilds
+        private double _balance = 0;
+
+        //Methods
+        public void Deposit(double depositAmmount)
+        {
+            _balance += depositAmmount;
+        }
+
+        public double GetBalance() 
+        { 
+            return _balance;
+        }
+    }
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -1,10 +1,44 @@
-﻿namespace BankEncapsulation
+﻿using System;
+
+namespace BankEncapsulation
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, ATM");
+            Console.WriteLine("");
+            Console.WriteLine("1 - Deposit $100");
+            Console.WriteLine("2 - Deposit $200");
+            Console.WriteLine("3 - Deposit $300");
+            Console.WriteLine("");
+            Console.WriteLine("Please Choose an Option 1-3");
+
+            var customer = new BankAccount();
+
+            var option = Console.ReadLine();
+
+            switch (option)
+            {
+                case "1":
+                    customer.Deposit(100.01);
+                    break;
+                case "2":
+                    customer.Deposit(200.00);
+                    break;
+                case "3":
+                    customer.Deposit(300.00);
+                    break;
+                default:
+                    Console.WriteLine("Invalid Entry. Please Start Over.");
+                    break;
+            }
+
+
+            var currentBalance = customer.GetBalance();
+            string currentBalanceLine = currentBalance.ToString();
+            Console.WriteLine($"Your deposit was successful. Your current Balance is now: ${currentBalanceLine}");
+
         }
     }
 }
